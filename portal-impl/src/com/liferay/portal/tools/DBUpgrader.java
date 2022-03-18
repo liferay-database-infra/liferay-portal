a0be314e3b035ad17c4e01021739c106f7fc8f27
Luis Ortiz <luis.ortiz@liferay.com>
2022-03-18 12:26:09 +0100

    LPS-147867 New methods for managing locks on the upgrade process

--- portal-impl/src/com/liferay/portal/tools/DBUpgrader.java
+++ portal-impl/src/com/liferay/portal/tools/DBUpgrader.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.cache.CacheRegistryUtil;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
 import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
@@ -33,6 +34,7 @@ import com.liferay.portal.kernel.model.ReleaseConstants;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.module.util.ServiceLatch;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.security.SecureRandomUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ReleaseInfo;
@@ -55,6 +57,8 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.logging.log4j.core.Appender;
@@ -67,6 +71,7 @@ import org.springframework.context.ApplicationContext;
 /**
  * @author Michael C. Han
  * @author Brian Wing Shun Chan
+ * @author Luis Ortiz
  */
 public class DBUpgrader {
 
@@ -195,6 +200,97 @@ public class DBUpgrader {
 		verifyResourcePermissions.verify();
 	}
 
+	public static void waitForLocks() throws Exception {
+		if (!PropsValues.UPGRADE_DATABASE_MANAGED_STARTUP || !_hasLockTable()) {
+			return;
+		}
+
+		while (true) {
+			try (Connection connection = DataAccess.getConnection();
+				PreparedStatement preparedStatement =
+					_getSelectLockPreparedStatement(connection);
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				if (resultSet.next()) {
+					Timestamp expirationDate = resultSet.getTimestamp(2);
+					Timestamp now = new Timestamp(System.currentTimeMillis());
+
+					if (expirationDate.before(now)) {
+						long lockId = resultSet.getLong(1);
+
+						try (PreparedStatement preparedStatement2 =
+								_getDeleteLockPreparedStatement(
+									connection, lockId)) {
+
+							preparedStatement2.executeUpdate();
+						}
+					}
+				}
+				else {
+					return;
+				}
+			}
+
+			Thread.sleep(PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME);
+		}
+	}
+
+	private static Thread _acquireLock(String owner) throws Exception {
+		if (!_hasLockTable()) {
+			return null;
+		}
+
+		while (true) {
+			try (Connection connection = DataAccess.getConnection();
+				PreparedStatement preparedStatement =
+					_getSelectLockPreparedStatement(connection);
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				if (resultSet.next()) {
+					Timestamp expirationDate = resultSet.getTimestamp(2);
+					Timestamp now = new Timestamp(System.currentTimeMillis());
+
+					if (expirationDate.before(now)) {
+						long lockId = resultSet.getLong(1);
+
+						try (PreparedStatement preparedStatement2 =
+								_getDeleteLockPreparedStatement(
+									connection, lockId)) {
+
+							preparedStatement2.executeUpdate();
+						}
+					}
+					else {
+						Thread.sleep(
+							PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME);
+
+						continue;
+					}
+				}
+			}
+
+			try (Connection connection = DataAccess.getConnection();
+				PreparedStatement preparedStatement =
+					_getInsertLockPreparedStatement(connection, owner)) {
+
+				if (preparedStatement.executeUpdate() == 1) {
+					Thread thread = new RefreshUpgradeLockThread(owner);
+
+					thread.start();
+
+					return thread;
+				}
+			}
+			catch (SQLException sqlException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(sqlException);
+				}
+
+				Thread.sleep(PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME);
+			}
+		}
+	}
+
 	private static void _checkClassNamesAndResourceActions() {
 		if (_log.isDebugEnabled()) {
 			_log.debug("Check class names");
@@ -226,6 +322,57 @@ public class DBUpgrader {
 		return buildNumber;
 	}
 
+	private static PreparedStatement _getDeleteLockPreparedStatement(
+			Connection connection, long lockId)
+		throws SQLException {
+
+		PreparedStatement preparedStatement = connection.prepareStatement(
+			"delete from Lock_ where lockId = ?");
+
+		preparedStatement.setLong(1, lockId);
+
+		return preparedStatement;
+	}
+
+	private static PreparedStatement _getDeleteLockPreparedStatement(
+			Connection connection, String owner)
+		throws SQLException {
+
+		PreparedStatement preparedStatement = connection.prepareStatement(
+			"delete from Lock_ where className = ? and key_ = ? and owner = ?");
+
+		preparedStatement.setString(1, DBUpgrader.class.getName());
+		preparedStatement.setString(2, _LOCK_KEY);
+		preparedStatement.setString(3, owner);
+
+		return preparedStatement;
+	}
+
+	private static PreparedStatement _getInsertLockPreparedStatement(
+			Connection connection, String owner)
+		throws SQLException {
+
+		PreparedStatement preparedStatement = connection.prepareStatement(
+			"insert into Lock_ (lockId, createDate, className, key_, owner, " +
+				"expirationDate) values (?, ?, ?, ?, ?, ?)");
+
+		long lockId = SecureRandomUtil.nextLong();
+
+		Timestamp now = new Timestamp(System.currentTimeMillis());
+
+		Timestamp expirationDate = new Timestamp(
+			now.getTime() + PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME);
+
+		preparedStatement.setLong(1, lockId);
+		preparedStatement.setTimestamp(2, now);
+		preparedStatement.setString(3, DBUpgrader.class.getName());
+		preparedStatement.setString(4, _LOCK_KEY);
+		preparedStatement.setString(5, owner);
+		preparedStatement.setTimestamp(6, expirationDate);
+
+		return preparedStatement;
+	}
+
 	private static int _getReleaseColumnValue(String columnName)
 		throws Exception {
 
@@ -248,6 +395,50 @@ public class DBUpgrader {
 		}
 	}
 
+	private static PreparedStatement _getSelectLockPreparedStatement(
+			Connection connection)
+		throws SQLException {
+
+		PreparedStatement preparedStatement = connection.prepareStatement(
+			"select lockId, expirationDate from Lock_ where className = ? " +
+				"and key_ = ?");
+
+		preparedStatement.setString(1, DBUpgrader.class.getName());
+		preparedStatement.setString(2, _LOCK_KEY);
+
+		return preparedStatement;
+	}
+
+	private static PreparedStatement _getUpdateLockPreparedStatement(
+			Connection connection, String owner)
+		throws SQLException {
+
+		PreparedStatement preparedStatement = connection.prepareStatement(
+			"update Lock_ set expirationDate = ? where className = ? and " +
+				"key_ = ? and owner = ?");
+
+		Timestamp now = new Timestamp(System.currentTimeMillis());
+
+		Timestamp expirationDate = new Timestamp(
+			now.getTime() + PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME);
+
+		preparedStatement.setTimestamp(1, expirationDate);
+
+		preparedStatement.setString(2, DBUpgrader.class.getName());
+		preparedStatement.setString(3, _LOCK_KEY);
+		preparedStatement.setString(4, owner);
+
+		return preparedStatement;
+	}
+
+	private static boolean _hasLockTable() throws Exception {
+		try (Connection connection = DataAccess.getConnection()) {
+			DBInspector dbInspector = new DBInspector(connection);
+
+			return dbInspector.hasTable("Lock_");
+		}
+	}
+
 	private static void _registerModuleServiceLifecycle(
 		String moduleServiceLifecycle) {
 
@@ -264,6 +455,27 @@ public class DBUpgrader {
 			).put(
 				"service.version", ReleaseInfo.getVersion()
 			).build());
+	}
+
+	private static void _releaseLock(Thread thread, String owner)
+		throws Exception {
+
+		if (!_hasLockTable()) {
+			return;
+		}
+
+		if (thread != null) {
+			thread.interrupt();
+
+			thread.join();
+		}
+
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement preparedStatement =
+				_getDeleteLockPreparedStatement(connection, owner)) {
+
+			preparedStatement.executeUpdate();
+		}
 	}
 
 	private static void _startUpgradeReportLogAppender() {
@@ -419,6 +631,8 @@ public class DBUpgrader {
 		verify();
 	}
 
+	private static final String _LOCK_KEY = "UpgradeProcess";
+
 	private static final Version _VERSION_7010 = new Version(0, 0, 6);
 
 	private static final Log _log = LogFactoryUtil.getLog(DBUpgrader.class);
@@ -426,5 +640,56 @@ public class DBUpgrader {
 	private static volatile Appender _appender;
 	private static volatile ServiceReference<Appender>
 		_appenderServiceReference;
+
+	private static class RefreshUpgradeLockThread extends Thread {
+
+		@Override
+		public void run() {
+			try {
+				while (!isInterrupted()) {
+					try (Connection connection = DataAccess.getConnection();
+						PreparedStatement preparedStatement =
+							_getUpdateLockPreparedStatement(
+								connection, _owner)) {
+
+						if (preparedStatement.executeUpdate() == 1) {
+							Thread.sleep(
+								PropsValues.UPGRADE_DATABASE_LOCK_REFRESH_TIME -
+									Time.SECOND);
+						}
+						else {
+							interrupt();
+						}
+					}
+					catch (SQLException sqlException) {
+						if (_log.isDebugEnabled()) {
+							_log.debug(sqlException);
+						}
+
+						interrupt();
+					}
+				}
+			}
+			catch (InterruptedException interruptedException) {
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"Stopped refresh upgrade database lock",
+						interruptedException);
+				}
+
+				interrupt();
+			}
+		}
+
+		private RefreshUpgradeLockThread(String owner) {
+			_owner = owner;
+
+			setDaemon(true);
+			setName("Refresh upgrade database lock");
+		}
+
+		private final String _owner;
+
+	}
 
 }
