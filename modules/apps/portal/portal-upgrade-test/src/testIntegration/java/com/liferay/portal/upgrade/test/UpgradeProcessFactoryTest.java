2385d0f1a6a9748f0422ebb559eafb7e14eadfee
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-02-21 13:23:55 +0100

    LPS-174324 Add testDropTables and testDropTableIfItDoesNotExist in UpgradeProcessFactoryTest

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeProcessFactoryTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeProcessFactoryTest.java
@@ -130,6 +130,30 @@ public class UpgradeProcessFactoryTest {
 	}
 
 	@Test
+	public void testDropTables() throws Exception {
+		_db.runSQL(
+			StringBundler.concat(
+				"create table ", "UpgradeProcessFactoryTest_2",
+				" (id LONG not null primary key);"));
+
+		UpgradeProcess upgradeProcess = UpgradeProcessFactory.dropTables(
+			_TABLE_NAME, "UpgradeProcessFactoryTest_2");
+
+		upgradeProcess.upgrade();
+
+		Assert.assertFalse(_dbInspector.hasTable(_TABLE_NAME, false));
+		Assert.assertFalse(_dbInspector.hasTable("UpgradeProcessFactoryTest_2", false));
+	}
+
+	@Test
+	public void testDropTableIfItDoesNotExist() throws Exception {
+		UpgradeProcess upgradeProcess = UpgradeProcessFactory.dropTables(
+			"UpgradeProcessFactoryTest_2");
+
+		upgradeProcess.upgrade();
+	}
+
+	@Test
 	public void testUtilOnPostUgpradeSteps() throws Exception {
 		UpgradeProcess upgradeProcess = new UpgradeProcess() {
 
