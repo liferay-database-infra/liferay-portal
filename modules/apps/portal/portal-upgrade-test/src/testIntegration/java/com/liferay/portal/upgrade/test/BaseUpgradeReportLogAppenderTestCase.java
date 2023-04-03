023cf629df0c14827712c8d461c1c78a7bc5a69d
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-04-03 16:50:37 +0200

    LPS-171285 SF

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseUpgradeReportLogAppenderTestCase.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseUpgradeReportLogAppenderTestCase.java
@@ -180,7 +180,7 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 				Assert.assertEquals(1, finalTableCount);
 			}
 			else if (StringUtil.equalsIgnoreCase(
-				tableName, "UpgradeReportTable2")) {
+						tableName, "UpgradeReportTable2")) {
 
 				table2Exists = true;
 
@@ -221,7 +221,8 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 				public void run() {
 					try {
 						long timeout = ReflectionTestUtil.getFieldValue(
-							PropsValues.class, "UPGRADE_REPORT_DL_STORAGE_INFO_TIMEOUT");
+							PropsValues.class,
+							"UPGRADE_REPORT_DL_STORAGE_INFO_TIMEOUT");
 
 						sleep(timeout + 5);
 					}
@@ -236,7 +237,7 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		_assertReport(
 			"Unable to determine the document library storage size because " +
-			"it is too large. You can check it manually");
+				"it is too large. You can check it manually");
 	}
 
 	@Test
@@ -262,10 +263,11 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 			});
 
 		_appender.stop();
-		
-			_assertReport(LanguageUtil.formatStorageSize(1073742000,
-				LocaleUtil.US));
+
+			_assertReport(
+				LanguageUtil.formatStorageSize(1073742000, LocaleUtil.US));
 		}
+
 	}
 
 	@Test
@@ -292,15 +294,9 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		_appender.stop();
 
-<<<<<<< HEAD
 		_assertReport(
-			"The document library storage size is " +
-			LanguageUtil.formatStorageSize(1048576, LocaleUtil.US));
-=======
-			_assertReport(LanguageUtil.formatStorageSize(1048576,
-				LocaleUtil.US));
-		}
->>>>>>> 83ef2de90136 (LPS-171285 tmp)
+				LanguageUtil.formatStorageSize(1048576, LocaleUtil.US));
+
 	}
 
 	@Test
@@ -314,14 +310,14 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		log.info(
 			"Completed upgrade process " + fasterUpgradeProcessName +
-			" in 10 ms");
+				" in 10 ms");
 
 		String slowerUpgradeProcessName =
 			"com.liferay.portal.SlowerUpgradeTest";
 
 		log.info(
 			"Completed upgrade process " + slowerUpgradeProcessName +
-			" in 20401 ms");
+				" in 20401 ms");
 
 		_appender.stop();
 
@@ -329,14 +325,14 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		Assert.assertTrue(
 			reportContent.indexOf(slowerUpgradeProcessName) <
-			reportContent.indexOf(fasterUpgradeProcessName));
+				reportContent.indexOf(fasterUpgradeProcessName));
 
 		String longestUpgradeProcessesValue = _getLogContextValue(
 			"upgrade.report.longest.upgrade.processes");
 
 		Assert.assertTrue(
 			longestUpgradeProcessesValue.indexOf(slowerUpgradeProcessName) <
-			longestUpgradeProcessesValue.indexOf(fasterUpgradeProcessName));
+				longestUpgradeProcessesValue.indexOf(fasterUpgradeProcessName));
 	}
 
 	@Test
@@ -353,7 +349,7 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		log.info(
 			"Completed upgrade process com.liferay.portal.UpgradeTest in " +
-			"20401 ms");
+				"20401 ms");
 
 		_appender.stop();
 
@@ -574,7 +570,7 @@ public abstract class BaseUpgradeReportLogAppenderTestCase {
 
 		Pattern pattern = Pattern.compile(
 			"(?s)INFO - Upgrade report generated in " + file.getAbsolutePath() +
-			"\\n\\s+\\{(.+)\\}");
+				"\\n\\s+\\{(.+)\\}");
 
 		Matcher matcher = pattern.matcher(_getLogContextContent());
 
