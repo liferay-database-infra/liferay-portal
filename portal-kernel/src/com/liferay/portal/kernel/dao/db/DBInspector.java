8117a1632c9ceca794978abbf92ad783aacb01f9
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-05-31 13:03:08 +0200

    LPS-181918 Supporting all db

--- portal-kernel/src/com/liferay/portal/kernel/dao/db/DBInspector.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/DBInspector.java
@@ -141,7 +141,8 @@ public class DBInspector {
 				String actualColumnDefaultValue = resultSet.getString(
 					"COLUMN_DEF");
 
-				_log.error("actualColumnDefaultValue: " + actualColumnDefaultValue);
+				_log.error(
+					"actualColumnDefaultValue: " + actualColumnDefaultValue);
 
 				if (actualColumnDefaultValue != null) {
 					Matcher matcher = _columnDbDefaultTypePattern.matcher(
@@ -355,8 +356,11 @@ public class DBInspector {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(DBInspector.class);
+
 	private static final Pattern _columnDbDefaultTypePattern = Pattern.compile(
-		"('?)(.*)\\1.*", Pattern.CASE_INSENSITIVE);
+		"^(\\()?(?(1)(\\()?)(?(2)|(')?)([^\\(\\)\\d]+|\\d+)(?(3)')(?(2)\\))(?" +
+			"(1)\\))(?(1)|(?(2)|(?(3)(::[^\\(\\)\\d]+)?| ?)))$",
+		Pattern.CASE_INSENSITIVE);
 	private static final Pattern _columnDefaultTypePattern = Pattern.compile(
 		".*DEFAULT '?(.*[^'])'? NOT NULL", Pattern.CASE_INSENSITIVE);
 	private static final Pattern _columnSizePattern = Pattern.compile(
