242714482b3f822d7e083bd839a8d812c409967e
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-07-24 12:12:07 +0200

    LPS-190855 No longer needed

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -377,11 +377,6 @@ public abstract class BaseDB implements DB {
 	}
 
 	@Override
-	public Integer getSQLVarcharSize(String templateType) {
-		return _sqlVarcharSizes.get(templateType);
-	}
-
-	@Override
 	public String getTemplateBlob() {
 		return getTemplate()[5];
 	}
@@ -1489,7 +1484,6 @@ public abstract class BaseDB implements DB {
 	private final int _minorVersion;
 	private final Map<String, Integer> _sqlTypes = new HashMap<>();
 	private final Map<String, Integer> _sqlTypeSizes = new HashMap<>();
-	private final Map<String, Integer> _sqlVarcharSizes = new HashMap<>();
 	private boolean _supportsStringCaseSensitiveQuery = true;
 	private final Map<String, String> _templates = new HashMap<>();
 
