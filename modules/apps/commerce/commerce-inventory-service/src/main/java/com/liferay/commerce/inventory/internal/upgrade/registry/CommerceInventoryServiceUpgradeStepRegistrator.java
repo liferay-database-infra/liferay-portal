627e3f4d4b061a25e5a83ece3e45985722693ca2
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-07-26 09:51:28 +0200

    LPS-190855 Undo workaround

--- modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/internal/upgrade/registry/CommerceInventoryServiceUpgradeStepRegistrator.java
+++ modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/internal/upgrade/registry/CommerceInventoryServiceUpgradeStepRegistrator.java
@@ -161,7 +161,9 @@ public class CommerceInventoryServiceUpgradeStepRegistrator
 				"unitOfMeasureKey VARCHAR(75) null"));
 
 		registry.register(
-			"2.7.0", "2.8.0", new CommerceInventoryAuditUpgradeProcess());
+			"2.7.0", "2.8.0",
+			UpgradeProcessFactory.alterColumnType(
+				"CIAudit", "quantity", "BIGDECIMAL null"));
 
 		if (_log.isInfoEnabled()) {
 			_log.info("Commerce inventory upgrade step registrator finished");
