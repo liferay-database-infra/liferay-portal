abf90619382c030daa972bec3619628396d6b9e6
Kevin Lee <kevin.lee@liferay.com>
2023-07-27 19:12:31 -0700

    LPS-191724 Ignore case when checking for 'NOT NULL' in DB2

--- modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/DB2DB.java
+++ modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/DB2DB.java
@@ -131,7 +131,7 @@ public class DB2DB extends BaseDB {
 
 			String tempColumnName = "temp" + columnName;
 
-			if (newColumnType.endsWith("not null") &&
+			if (StringUtil.endsWith(newColumnType, "not null") &&
 				(dbInspector.getColumnDefaultValue(newColumnType) == null)) {
 
 				runSQL(
