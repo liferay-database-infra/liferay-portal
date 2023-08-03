68f608c3e9648cbd3409760c17e0162f558c3268
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-08-03 17:52:12 +0200

    LPS-185207 ClassName must be considered as sharded model even when does not have companyId

--- modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/FinderCacheImpl.java
+++ modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/FinderCacheImpl.java
@@ -30,7 +30,6 @@ import com.liferay.portal.kernel.db.partition.DBPartition;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
-import com.liferay.portal.kernel.model.ShardedModel;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LRUMap;
@@ -622,8 +621,7 @@ public class FinderCacheImpl
 					Class<?> modelImplClass = classLoader.loadClass(
 						argumentsResolver.getClassName());
 
-					sharded = ShardedModel.class.isAssignableFrom(
-						modelImplClass);
+					sharded = DBPartition.isPartitionedModel(modelImplClass);
 				}
 				catch (ClassNotFoundException classNotFoundException) {
 					if (_log.isWarnEnabled()) {
