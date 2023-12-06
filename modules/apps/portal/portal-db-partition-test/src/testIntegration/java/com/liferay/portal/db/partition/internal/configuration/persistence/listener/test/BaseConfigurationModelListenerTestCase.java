2685877378d4ad75f3f0ec72c3c07df6e0c29203
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-12-06 16:34:41 +0100

    LPS-194500 Apply new methods to setUp/tearDown partitions.
    # Conflicts:
    #	modules/apps/on-demand-admin/on-demand-admin-test/src/testIntegration/java/com/liferay/on/demand/admin/ticket/generator/test/OnDemandAdminTicketGeneratorDBPartitionTest.java

--- modules/apps/portal/portal-db-partition-test/src/testIntegration/java/com/liferay/portal/db/partition/internal/configuration/persistence/listener/test/BaseConfigurationModelListenerTestCase.java
+++ modules/apps/portal/portal-db-partition-test/src/testIntegration/java/com/liferay/portal/db/partition/internal/configuration/persistence/listener/test/BaseConfigurationModelListenerTestCase.java
@@ -48,20 +48,12 @@ public abstract class BaseConfigurationModelListenerTestCase
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		enableDBPartition();
-
-		addDBPartitions();
-
-		insertPartitionRequiredData();
+		setUpDBPartitions();
 	}
 
 	@AfterClass
 	public static void tearDownClass() throws Exception {
-		deletePartitionRequiredData();
-
-		removeDBPartitions();
-
-		disableDBPartition();
+		tearDownDBPartitions();
 	}
 
 	@After
