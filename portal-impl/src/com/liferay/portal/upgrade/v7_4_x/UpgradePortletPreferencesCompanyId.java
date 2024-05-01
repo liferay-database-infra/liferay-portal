05901bac74737c9fb7a06122ff3188f477260bac
IstvanD <istvan.dezsi@liferay.com>
2024-05-01 21:27:42 +0200

    LPD-20634 Adding new upgrade process to to populate the companyId for the remaining portletPreferences and portletPreferenceValues

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradePortletPreferencesCompanyId.java
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.v7_4_x;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeCompanyId;
+
+/**
+ * @author István András Dézsi
+ */
+public class UpgradePortletPreferencesCompanyId extends UpgradeCompanyId {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		super.doUpgrade();
+
+		runSQL(
+			StringBundler.concat(
+				"update PortletPreferenceValue set companyId = (select ",
+				"PortletPreferences.companyId from PortletPreferences where ",
+				"PortletPreferences.portletPreferencesId = ",
+				"PortletPreferenceValue.portletPreferencesId)"));
+	}
+
+	@Override
+	protected TableUpdater[] getTableUpdaters() {
+		return new TableUpdater[] {
+			new PortletPreferencesTableUpdater("PortletPreferences")
+		};
+	}
+
+}
