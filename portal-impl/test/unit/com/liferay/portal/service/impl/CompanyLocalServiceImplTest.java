5816cb0af15703c83848a421ea4506a302332740
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-05-14 08:25:16 +0200

    LPD-22964 Move to impl package for consistency.

--- portal-impl/test/unit/com/liferay/portal/service/impl/CompanyLocalServiceImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/service/impl/CompanyLocalServiceImplTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.service;
+package com.liferay.portal.service.impl;
 
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -12,7 +12,6 @@ import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.model.impl.CompanyImpl;
-import com.liferay.portal.service.impl.CompanyLocalServiceImpl;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.util.PropsValues;
 
