8e77857ebd137a1e362c95591ca131e700270579
IstvanD <istvan.dezsi@liferay.com>
2024-05-16 12:13:43 +0200

    LPD-23831 Removing the companyId from the messageContent

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTMessageLocalServiceImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTMessageLocalServiceImpl.java
@@ -9,7 +9,12 @@ import com.liferay.change.tracking.model.CTMessage;
 import com.liferay.change.tracking.service.base.CTMessageLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.change.tracking.CTAware;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 
 import java.util.ArrayList;
@@ -37,7 +42,9 @@ public class CTMessageLocalServiceImpl extends CTMessageLocalServiceBaseImpl {
 		CTMessage ctMessage = ctMessagePersistence.create(ctMessageId);
 
 		ctMessage.setCtCollectionId(ctCollectionId);
-		ctMessage.setMessageContent(_jsonFactory.serialize(message));
+
+		ctMessage.setMessageContent(
+			_removeCompanyId(_jsonFactory.serialize(message)));
 
 		return ctMessagePersistence.update(ctMessage);
 	}
@@ -61,6 +68,53 @@ public class CTMessageLocalServiceImpl extends CTMessageLocalServiceBaseImpl {
 
 		return messages;
 	}
+
+	private JSONObject _removeCompanyId(JSONObject jsonObject) {
+		if (jsonObject == null) {
+			return null;
+		}
+
+		jsonObject.remove("companyId");
+
+		for (String key : jsonObject.keySet()) {
+			Object value = jsonObject.get(key);
+
+			if (value instanceof JSONObject) {
+				_removeCompanyId((JSONObject)value);
+			}
+			else if (value instanceof JSONArray) {
+				JSONArray jsonArray = (JSONArray)value;
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					Object arrayElement = jsonArray.get(i);
+
+					if (arrayElement instanceof JSONObject) {
+						_removeCompanyId((JSONObject)arrayElement);
+					}
+				}
+			}
+		}
+
+		return jsonObject;
+	}
+
+	private String _removeCompanyId(String json) {
+		try {
+			JSONObject jsonObject = _jsonFactory.createJSONObject(json);
+
+			json = String.valueOf(_removeCompanyId(jsonObject));
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException);
+			}
+		}
+
+		return json;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CTMessageLocalServiceImpl.class);
 
 	@Reference
 	private JSONFactory _jsonFactory;
