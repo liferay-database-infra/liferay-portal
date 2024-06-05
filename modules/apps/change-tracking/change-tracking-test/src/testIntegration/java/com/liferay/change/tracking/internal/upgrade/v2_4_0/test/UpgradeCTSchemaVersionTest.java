8b67498cb76ea69b7511fb021dd040905e6f2d2c
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-06-05 15:59:19 +0200

    LPD-23831 Move the upgrade tests to internal

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/upgrade/v2_4_0/test/UpgradeCTSchemaVersionTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/upgrade/v2_4_0/test/UpgradeCTSchemaVersionTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.change.tracking.upgrade.v2_4_0.test;
+package com.liferay.change.tracking.internal.upgrade.v2_4_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.change.tracking.constants.CTConstants;
