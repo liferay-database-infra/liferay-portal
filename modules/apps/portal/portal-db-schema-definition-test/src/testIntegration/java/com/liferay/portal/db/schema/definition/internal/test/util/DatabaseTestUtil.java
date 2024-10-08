1b2d049ed8be62f101bc950ff375ec885614eb64
Luis Ortiz <luis.ortiz@liferay.com>
2024-10-08 11:56:15 +0200

    LPD-33358 Revert "LPD-23982 No need to compare index names until LPD-33358 gets implemented."
    This reverts commit ad3ae52925f1748df7c9883fb0da7ab32afc395c.

--- modules/apps/portal/portal-db-schema-definition-test/src/testIntegration/java/com/liferay/portal/db/schema/definition/internal/test/util/DatabaseTestUtil.java
+++ modules/apps/portal/portal-db-schema-definition-test/src/testIntegration/java/com/liferay/portal/db/schema/definition/internal/test/util/DatabaseTestUtil.java
@@ -193,6 +193,7 @@ public class DatabaseTestUtil {
 					StringUtil.merge(
 						new Object[] {
 							tableName, resultSet.getString("NON_UNIQUE"),
+							resultSet.getString("INDEX_NAME"),
 							resultSet.getString("COLUMN_NAME"),
 							resultSet.getShort("ORDINAL_POSITION")
 						}));
