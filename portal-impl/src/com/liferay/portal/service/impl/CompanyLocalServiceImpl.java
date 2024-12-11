5d260ab4cb0bf209e97beb94317bb6aa2f43833e
IstvanD <istvan.dezsi@liferay.com>
2024-12-11 16:04:37 +0100

    LPD-44096 Clean up company related data from tables upon deletion

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -22,6 +22,10 @@ import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
 import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 import com.liferay.portal.kernel.cluster.ClusterRequest;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Disjunction;
 import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
@@ -140,6 +144,8 @@ import java.net.IDN;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import java.sql.Connection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -2390,6 +2396,29 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	private void _cleanupCompanyData(long companyId) {
+		try (Connection connection = DataAccess.getConnection()) {
+			DB db = DBManagerUtil.getDB();
+
+			DBInspector dbInspector = new DBInspector(connection);
+
+			for (String tableName : dbInspector.getTableNames(null)) {
+				if (dbInspector.hasColumn(tableName, "companyId") &&
+					!tableName.equals("company")) {
+
+					db.runSQL(
+						connection,
+						StringBundler.concat(
+							"delete from ", tableName, " where companyId = ",
+							companyId));
+				}
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+	}
+
 	private void _clearCompanyCache(long companyId, boolean removePortalCache) {
 		Company company = companyPersistence.fetchByPrimaryKey(companyId);
 
@@ -2460,6 +2489,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 				unregisterCompany(company);
 
 				_synchronizePortalInstances();
+
+				_cleanupCompanyData(company.getCompanyId());
 
 				return null;
 			});
