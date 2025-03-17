8ed82541836700f7e26d67b15b183cee28aa960c
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-03-17 19:42:24 +0100

    Revert "LPD-45633 Moving _isProcessed logic to the top of the method invoking"
    This reverts commit 9522bb12d63998f33a22822620229a466e738881.

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/unit/BatchEngineUnitProcessorImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/unit/BatchEngineUnitProcessorImpl.java
@@ -361,10 +361,6 @@ public class BatchEngineUnitProcessorImpl implements BatchEngineUnitProcessor {
 			CompletableFuture<Void> completableFuture)
 		throws Exception {
 
-		if (_isProcessed(batchEngineUnit)) {
-			return null;
-		}
-
 		BatchEngineUnitConfiguration batchEngineUnitConfiguration = null;
 		byte[] content = null;
 		String contentType = null;
@@ -398,6 +394,10 @@ public class BatchEngineUnitProcessorImpl implements BatchEngineUnitProcessor {
 				StringBundler.concat(
 					"Invalid batch engine file ", batchEngineUnit.getFileName(),
 					" ", batchEngineUnit.getDataFileName()));
+		}
+
+		if (_isProcessed(batchEngineUnit)) {
+			return null;
 		}
 
 		return _execute(
