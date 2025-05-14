cff595eb293727901da151b3da3cd5edf4df27f4
Luis Ortiz <luis.ortiz@liferay.com>
2025-05-14 13:44:15 +0200

    LPD-52637 Moving logic that can be reused for JakartaUpgrade class integration tests to a new BaseTestCase class

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseJakartaUpgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseJakartaUpgradeProcessTest.java
@@ -6,38 +6,9 @@
 package com.liferay.portal.upgrade.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBManagerUtil;
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.db.partition.DBPartition;
-import com.liferay.portal.kernel.instance.PortalInstancePool;
-import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.upgrade.BaseJakartaUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.test.log.LogCapture;
-import com.liferay.portal.test.log.LogEntry;
-import com.liferay.portal.test.log.LoggerTestUtil;
-import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -45,204 +16,65 @@ import org.junit.runner.RunWith;
  * @author Luis Ortiz
  */
 @RunWith(Arquillian.class)
-public class BaseJakartaUpgradeProcessTest extends BaseJakartaUpgradeProcess {
-
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
-
-	@Before
-	public void setUp() throws Exception {
-		_db = DBManagerUtil.getDB();
-
-		_companyLocalService.forEachCompany(
-			company -> _db.runSQL(
-				StringBundler.concat(
-					"create table ", _TABLE_NAME,
-					" (mvccVersion LONG default 0 not null, uuid_ VARCHAR(75) ",
-					"not null, ", _COLUMN_NAME_1, " TEXT null, ",
-					_COLUMN_NAME_2, " VARCHAR(255) null, ", _COLUMN_NAME_3,
-					" STRING null, primary key (mvccVersion, uuid_))")));
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		_companyLocalService.forEachCompany(
-			company -> _db.runSQL("drop table " + _TABLE_NAME));
-	}
+public class BaseJakartaUpgradeProcessTest extends BaseJakartaUpgradeTestCase {
 
 	@Test
 	public void testUpgradeWithCustomSeparators() throws Exception {
-		_initialString = "import javax$portlet$Portlet";
-		_resultString = "import jakarta$portlet$Portlet";
+		initialString = "import javax$portlet$Portlet";
+		resultString = "import jakarta$portlet$Portlet";
 
-		_insertInitialData();
+		insertInitialData();
 
-		_testUpgrade(
-			new BaseJakartaUpgradeProcessTest() {
+		testUpgrade(
+			() -> {
+				UpgradeProcess upgradeProcess =
+					new BaseJakartaUpgradeProcess() {
 
-				@Override
-				public char[] getCustomSeparators() {
-					return new char[] {'$'};
-				}
+						@Override
+						public char[] getCustomSeparators() {
+							return new char[] {'$'};
+						}
 
+						@Override
+						protected String[][] getTableAndColumnNames() {
+							return new String[][] {
+								{TABLE_NAME, COLUMN_NAME_1},
+								{TABLE_NAME, COLUMN_NAME_2},
+								{TABLE_NAME, COLUMN_NAME_3}
+							};
+						}
+
+					};
+
+				upgradeProcess.upgrade();
 			});
 	}
 
 	@Test
 	public void testUpgradeWithoutCustomSeparators() throws Exception {
-		_initialString = "import javax.portlet.Portlet";
-		_resultString = "import jakarta.portlet.Portlet";
+		initialString = "import javax.portlet.Portlet";
+		resultString = "import jakarta.portlet.Portlet";
 
-		_insertInitialData();
+		insertInitialData();
 
-		_testUpgrade(this);
-	}
+		testUpgrade(
+			() -> {
+				UpgradeProcess upgradeProcess =
+					new BaseJakartaUpgradeProcess() {
 
-	@Override
-	protected String[][] getTableAndColumnNames() {
-		return new String[][] {
-			{_TABLE_NAME, _COLUMN_NAME_1}, {_TABLE_NAME, _COLUMN_NAME_2},
-			{_TABLE_NAME, _COLUMN_NAME_3}
-		};
-	}
+						@Override
+						protected String[][] getTableAndColumnNames() {
+							return new String[][] {
+								{TABLE_NAME, COLUMN_NAME_1},
+								{TABLE_NAME, COLUMN_NAME_2},
+								{TABLE_NAME, COLUMN_NAME_3}
+							};
+						}
 
-	private void _assertLogEntry(
-		String expectedMessage, Set<String> expectedKeys, String logEntry) {
+					};
 
-		Assert.assertTrue(logEntry, logEntry.contains(expectedMessage));
-
-		for (String key : expectedKeys) {
-			Assert.assertTrue(logEntry, logEntry.contains(key));
-		}
-	}
-
-	private void _insertInitialData() throws Exception {
-		_companyLocalService.forEachCompany(
-			company -> {
-				_db.runSQL(
-					StringBundler.concat(
-						"insert into ", _TABLE_NAME, " (mvccVersion, uuid_, ",
-						_COLUMN_NAME_1, ", ", _COLUMN_NAME_2, ", ",
-						_COLUMN_NAME_3, ") values (0, 'uuid1', '",
-						_initialString, "', '", _initialString, "', '",
-						_initialString, "')"));
-
-				_db.runSQL(
-					StringBundler.concat(
-						"insert into ", _TABLE_NAME, " (mvccVersion, uuid_, ",
-						_COLUMN_NAME_1, ", ", _COLUMN_NAME_2,
-						") values (1, 'uuid2', '", _initialString, "', '",
-						_initialString, "')"));
+				upgradeProcess.upgrade();
 			});
 	}
-
-	private void _testUpgrade(UpgradeProcess upgradeProcess) throws Exception {
-		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
-				BaseJakartaUpgradeProcess.class.getName(),
-				LoggerTestUtil.INFO)) {
-
-			upgradeProcess.upgrade();
-
-			try (Connection connection = DataAccess.getConnection();
-				PreparedStatement preparedStatement =
-					connection.prepareStatement(
-						"select * from " + _TABLE_NAME + " order by uuid_ asc");
-				ResultSet resultSet = preparedStatement.executeQuery()) {
-
-				Assert.assertTrue(resultSet.next());
-
-				Assert.assertEquals(0, resultSet.getLong(1));
-				Assert.assertEquals("uuid1", resultSet.getString(2));
-				Assert.assertEquals(
-					_resultString, resultSet.getString(_COLUMN_NAME_1));
-				Assert.assertEquals(
-					_resultString, resultSet.getString(_COLUMN_NAME_2));
-				Assert.assertEquals(
-					_resultString, resultSet.getString(_COLUMN_NAME_3));
-
-				Assert.assertTrue(resultSet.next());
-
-				Assert.assertEquals(1, resultSet.getLong(1));
-				Assert.assertEquals("uuid2", resultSet.getString(2));
-				Assert.assertEquals(
-					_resultString, resultSet.getString(_COLUMN_NAME_1));
-				Assert.assertEquals(
-					_resultString, resultSet.getString(_COLUMN_NAME_2));
-				Assert.assertNull(resultSet.getString(_COLUMN_NAME_3));
-
-				Assert.assertFalse(resultSet.next());
-			}
-
-			List<LogEntry> logEntries = logCapture.getLogEntries();
-
-			int logEntriesSize = 3;
-
-			if (DBPartition.isPartitionEnabled()) {
-				logEntriesSize =
-					logEntriesSize * PortalInstancePool.getCompanyIds().length;
-			}
-
-			Assert.assertEquals(
-				logEntries.toString(), logEntriesSize, logEntries.size());
-
-			AtomicInteger i = new AtomicInteger();
-
-			long[] companyIds = ReflectionTestUtil.invoke(
-				PortalInstancePool.class, "_getCompanyIdsBySQL", null);
-
-			String companyIdMessage = "";
-
-			for (long companyId : companyIds) {
-				if (DBPartition.isPartitionEnabled()) {
-					companyIdMessage = " for company " + companyId;
-				}
-
-				_assertLogEntry(
-					StringBundler.concat(
-						"Table/column ", _TABLE_NAME, "/", _COLUMN_NAME_1,
-						companyIdMessage, " has been upgraded for next IDs:"),
-					new HashSet<>(Arrays.asList("(0, uuid1)", "(1, uuid2)")),
-					logEntries.get(
-						i.getAndIncrement()
-					).toString());
-
-				_assertLogEntry(
-					StringBundler.concat(
-						"Table/column ", _TABLE_NAME, "/", _COLUMN_NAME_2,
-						companyIdMessage, " has been upgraded for next IDs: "),
-					new HashSet<>(Arrays.asList("(0, uuid1)", "(1, uuid2)")),
-					logEntries.get(
-						i.getAndIncrement()
-					).toString());
-
-				_assertLogEntry(
-					StringBundler.concat(
-						"Table/column ", _TABLE_NAME, "/", _COLUMN_NAME_3,
-						companyIdMessage, " has been upgraded for next IDs: "),
-					new HashSet<>(Arrays.asList("(0, uuid1)")),
-					logEntries.get(
-						i.getAndIncrement()
-					).toString());
-			}
-		}
-	}
-
-	private static final String _COLUMN_NAME_1 = "script1";
-
-	private static final String _COLUMN_NAME_2 = "script2";
-
-	private static final String _COLUMN_NAME_3 = "script3";
-
-	private static final String _TABLE_NAME = "BaseJakartaUpgradeProcessTest";
-
-	@Inject
-	private static CompanyLocalService _companyLocalService;
-
-	private static DB _db;
-
-	private String _initialString;
-	private String _resultString;
 
 }
---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseJakartaUpgradeTestCase.java
@@ -0,0 +1,209 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.db.partition.DBPartition;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.upgrade.BaseJakartaUpgradeProcess;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Luis Ortiz
+ */
+public abstract class BaseJakartaUpgradeTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		db = DBManagerUtil.getDB();
+
+		companyLocalService.forEachCompany(
+			company -> db.runSQL(
+				StringBundler.concat(
+					"create table ", TABLE_NAME,
+					" (mvccVersion LONG default 0 not null, uuid_ VARCHAR(75) ",
+					"not null, ", COLUMN_NAME_1, " TEXT null, ", COLUMN_NAME_2,
+					" VARCHAR(255) null, ", COLUMN_NAME_3,
+					" STRING null, primary key (mvccVersion, uuid_))")));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		companyLocalService.forEachCompany(
+			company -> db.runSQL("drop table " + TABLE_NAME));
+	}
+
+	protected void insertInitialData() throws Exception {
+		companyLocalService.forEachCompany(
+			company -> {
+				db.runSQL(
+					StringBundler.concat(
+						"insert into ", TABLE_NAME, " (mvccVersion, uuid_, ",
+						COLUMN_NAME_1, ", ", COLUMN_NAME_2, ", ", COLUMN_NAME_3,
+						") values (0, 'uuid1', '", initialString, "', '",
+						initialString, "', '", initialString, "')"));
+
+				db.runSQL(
+					StringBundler.concat(
+						"insert into ", TABLE_NAME, " (mvccVersion, uuid_, ",
+						COLUMN_NAME_1, ", ", COLUMN_NAME_2,
+						") values (1, 'uuid2', '", initialString, "', '",
+						initialString, "')"));
+			});
+	}
+
+	protected void testUpgrade(UnsafeRunnable<Exception> unsafeRunnable)
+		throws Exception {
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				BaseJakartaUpgradeProcess.class.getName(),
+				LoggerTestUtil.INFO)) {
+
+			unsafeRunnable.run();
+
+			try (Connection connection = DataAccess.getConnection();
+				PreparedStatement preparedStatement =
+					connection.prepareStatement(
+						"select * from " + TABLE_NAME + " order by uuid_ asc");
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				Assert.assertTrue(resultSet.next());
+
+				Assert.assertEquals(0, resultSet.getLong(1));
+				Assert.assertEquals("uuid1", resultSet.getString(2));
+				Assert.assertEquals(
+					resultString, resultSet.getString(COLUMN_NAME_1));
+				Assert.assertEquals(
+					resultString, resultSet.getString(COLUMN_NAME_2));
+				Assert.assertEquals(
+					resultString, resultSet.getString(COLUMN_NAME_3));
+
+				Assert.assertTrue(resultSet.next());
+
+				Assert.assertEquals(1, resultSet.getLong(1));
+				Assert.assertEquals("uuid2", resultSet.getString(2));
+				Assert.assertEquals(
+					resultString, resultSet.getString(COLUMN_NAME_1));
+				Assert.assertEquals(
+					resultString, resultSet.getString(COLUMN_NAME_2));
+				Assert.assertNull(resultSet.getString(COLUMN_NAME_3));
+
+				Assert.assertFalse(resultSet.next());
+			}
+
+			List<LogEntry> logEntries = logCapture.getLogEntries();
+
+			int logEntriesSize = 3;
+
+			if (DBPartition.isPartitionEnabled()) {
+				logEntriesSize =
+					logEntriesSize * PortalInstancePool.getCompanyIds().length;
+			}
+
+			Assert.assertEquals(
+				logEntries.toString(), logEntriesSize, logEntries.size());
+
+			AtomicInteger i = new AtomicInteger();
+
+			long[] companyIds = ReflectionTestUtil.invoke(
+				PortalInstancePool.class, "_getCompanyIdsBySQL", null);
+
+			String companyIdMessage = "";
+
+			for (long companyId : companyIds) {
+				if (DBPartition.isPartitionEnabled()) {
+					companyIdMessage = " for company " + companyId;
+				}
+
+				_assertLogEntry(
+					StringBundler.concat(
+						"Table/column ", TABLE_NAME, "/", COLUMN_NAME_1,
+						companyIdMessage, " has been upgraded for next IDs:"),
+					new HashSet<>(Arrays.asList("(0, uuid1)", "(1, uuid2)")),
+					logEntries.get(
+						i.getAndIncrement()
+					).toString());
+
+				_assertLogEntry(
+					StringBundler.concat(
+						"Table/column ", TABLE_NAME, "/", COLUMN_NAME_2,
+						companyIdMessage, " has been upgraded for next IDs: "),
+					new HashSet<>(Arrays.asList("(0, uuid1)", "(1, uuid2)")),
+					logEntries.get(
+						i.getAndIncrement()
+					).toString());
+
+				_assertLogEntry(
+					StringBundler.concat(
+						"Table/column ", TABLE_NAME, "/", COLUMN_NAME_3,
+						companyIdMessage, " has been upgraded for next IDs: "),
+					new HashSet<>(Arrays.asList("(0, uuid1)")),
+					logEntries.get(
+						i.getAndIncrement()
+					).toString());
+			}
+		}
+	}
+
+	protected static final String COLUMN_NAME_1 = "script1";
+
+	protected static final String COLUMN_NAME_2 = "script2";
+
+	protected static final String COLUMN_NAME_3 = "script3";
+
+	protected static final String TABLE_NAME = "BaseJakartaUpgradeProcessTest";
+
+	@Inject
+	protected static CompanyLocalService companyLocalService;
+
+	protected static DB db;
+
+	protected String initialString;
+	protected String resultString;
+
+	private void _assertLogEntry(
+		String expectedMessage, Set<String> expectedKeys, String logEntry) {
+
+		Assert.assertTrue(logEntry, logEntry.contains(expectedMessage));
+
+		for (String key : expectedKeys) {
+			Assert.assertTrue(logEntry, logEntry.contains(key));
+		}
+	}
+
+}
