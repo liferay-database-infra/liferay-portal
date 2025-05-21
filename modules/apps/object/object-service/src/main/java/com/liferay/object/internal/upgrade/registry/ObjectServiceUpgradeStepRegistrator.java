89d21bad7689b081073b948df9465db1a20c7cf0
Luis Ortiz <luis.ortiz@liferay.com>
2025-05-21 10:58:50 +0200

    LPD-52638 Adding upgrade process for object-service tables that can host javax values

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -20,6 +20,7 @@ import com.liferay.object.internal.upgrade.v10_8_0.util.ObjectDefinitionSettingT
 import com.liferay.object.internal.upgrade.v10_8_1.ObjectEntryAssetEntryTitleUpgradeProcess;
 import com.liferay.object.internal.upgrade.v10_9_0.util.ObjectEntryVersionTable;
 import com.liferay.object.internal.upgrade.v10_9_1.ClassNameUpgradeProcess;
+import com.liferay.object.internal.upgrade.v11_0_0.ObjectJakartaUpgradeProcess;
 import com.liferay.object.internal.upgrade.v1_2_0.util.ObjectViewColumnTable;
 import com.liferay.object.internal.upgrade.v1_2_0.util.ObjectViewTable;
 import com.liferay.object.internal.upgrade.v2_1_0.ObjectFieldBusinessTypeUpgradeProcess;
@@ -623,6 +624,9 @@ public class ObjectServiceUpgradeStepRegistrator
 			"10.17.0", "10.18.0",
 			new com.liferay.object.internal.upgrade.v10_18_0.
 				ObjectFieldUpgradeProcess());
+
+		registry.register(
+			"10.18.0", "11.0.0", new ObjectJakartaUpgradeProcess());
 	}
 
 	@Reference
