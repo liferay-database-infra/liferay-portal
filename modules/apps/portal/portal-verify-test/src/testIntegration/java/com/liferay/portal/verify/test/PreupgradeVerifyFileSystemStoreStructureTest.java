abf71557495cfd2f38581d64dc4c8f52cd698397
IstvanD <istvan.dezsi@liferay.com>
2025-06-13 11:46:35 +0200

    LPD-53149 Adding tests for file system store structure and permissions verifications

---
+++ modules/apps/portal/portal-verify-test/src/testIntegration/java/com/liferay/portal/verify/test/PreupgradeVerifyFileSystemStoreStructureTest.java
@@ -0,0 +1,348 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.verify.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.verify.PreupgradeVerifyFileSystemStoreStructure;
+import com.liferay.portal.verify.VerifyProcess;
+import com.liferay.portal.verify.test.util.BaseVerifyProcessTestCase;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author István András Dézsi
+ */
+@RunWith(Arquillian.class)
+public class PreupgradeVerifyFileSystemStoreStructureTest
+	extends BaseVerifyProcessTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_companyId = TestPropsValues.getCompanyId();
+
+		_repositoryId = RandomTestUtil.nextLong();
+
+		_advancedFileSystemStoreRootDir =
+			_props.get(PropsKeys.LIFERAY_HOME) +
+				"/test/store/advanced_file_system";
+
+		_advancedFileSystemStoreConfiguration =
+			_configurationAdmin.getConfiguration(
+				"com.liferay.portal.store.file.system.configuration." +
+					"AdvancedFileSystemStoreConfiguration",
+				StringPool.QUESTION);
+
+		ConfigurationTestUtil.saveConfiguration(
+			_advancedFileSystemStoreConfiguration,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"rootDir", _advancedFileSystemStoreRootDir
+			).build());
+
+		_fileSystemStoreRootDir =
+			_props.get(PropsKeys.LIFERAY_HOME) + "/test/store/file_system";
+
+		_fileSystemStoreConfiguration = _configurationAdmin.getConfiguration(
+			"com.liferay.portal.store.file.system.configuration." +
+				"FileSystemStoreConfiguration",
+			StringPool.QUESTION);
+
+		ConfigurationTestUtil.saveConfiguration(
+			_fileSystemStoreConfiguration,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"rootDir", _fileSystemStoreRootDir
+			).build());
+
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable =
+			PropsValuesTestUtil.swapWithSafeCloseable(
+				"UPGRADE_DATABASE_DL_STORAGE_CHECK_DISABLED", false);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		ConfigurationTestUtil.deleteConfiguration(
+			_advancedFileSystemStoreConfiguration);
+		ConfigurationTestUtil.deleteConfiguration(
+			_fileSystemStoreConfiguration);
+
+		FileUtil.deltree(_advancedFileSystemStoreRootDir);
+		FileUtil.deltree(_fileSystemStoreRootDir);
+
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable.close();
+	}
+
+	@After
+	public void tearDown() {
+		FileUtil.deltree(
+			Paths.get(
+				_fileSystemStoreRootDir, String.valueOf(_companyId)
+			).toString());
+		FileUtil.deltree(
+			Paths.get(
+				_advancedFileSystemStoreRootDir, String.valueOf(_companyId)
+			).toString());
+	}
+
+	@Test
+	public void testAdvancedFileSystemStoreWithLongDirectoryNameWithoutExtension()
+		throws Exception {
+
+		Path directoryWithoutExtension = Paths.get(
+			_advancedFileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId), "100");
+
+		String expectedMessage =
+			"Found directory with name longer than 2 without extension in " +
+				"advanced file system pattern directory: " +
+					directoryWithoutExtension;
+
+		_assertVerify(
+			_ADVANCED_FILE_SYSTEM_STORE, directoryWithoutExtension, null,
+			expectedMessage);
+	}
+
+	@Test
+	public void testAdvancedFileSystemStoreWithValidStructure()
+		throws Exception {
+
+		Path validDirectory = Paths.get(
+			_advancedFileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId), "10", "100.afsh");
+
+		_assertVerify(_ADVANCED_FILE_SYSTEM_STORE, validDirectory, null);
+	}
+
+	@Test
+	public void testFileSystemStoreWithDirectoryContainingExtension()
+		throws Exception {
+
+		Path directoryWithExtension = Paths.get(
+			_fileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId), "100.txt");
+
+		String expectedMessage =
+			"Found directory with extension in basic file system pattern (no " +
+				"extensions expected): " + directoryWithExtension;
+
+		_assertVerify(
+			_FILE_SYSTEM_STORE, directoryWithExtension, null, expectedMessage);
+	}
+
+	@Test
+	public void testFileSystemStoreWithFileInsteadOfFolderIdDirectory()
+		throws Exception {
+
+		Path companyIdPath = Paths.get(
+			_fileSystemStoreRootDir, String.valueOf(_companyId));
+
+		Path invalidFilePath = companyIdPath.resolve("invalidFile.txt");
+
+		String expectedMessage =
+			"Found file in basic file system pattern directory (only " +
+				"directories expected): " + invalidFilePath;
+
+		_assertVerify(
+			_FILE_SYSTEM_STORE, null, invalidFilePath, expectedMessage);
+	}
+
+	@Test
+	public void testFileSystemStoreWithFileInsteadOfNumericFileEntryNameDirectory()
+		throws Exception {
+
+		Path folderIdPath = Paths.get(
+			_fileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId));
+
+		Path invalidFilePath = folderIdPath.resolve("invalidFile.txt");
+
+		String expectedMessage =
+			"Found file in basic file system pattern directory (only " +
+				"directories expected): " + invalidFilePath;
+
+		_assertVerify(
+			_FILE_SYSTEM_STORE, null, invalidFilePath, expectedMessage);
+	}
+
+	@Test
+	public void testFileSystemStoreWithMissingValidVersionFiles()
+		throws Exception {
+
+		Path numericFileEntryNamePath = Paths.get(
+			_fileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId), "100");
+
+		Path invalidVersionFile = numericFileEntryNamePath.resolve(
+			"invalidVersion");
+
+		String expectedMessage1 =
+			"Found file that does not match version pattern (expected " +
+				"\\d+\\.\\d+.*): " + invalidVersionFile;
+
+		String expectedMessage2 =
+			"Directory does not contain valid version files as expected in " +
+				"basic file system pattern: " + numericFileEntryNamePath;
+
+		_assertVerify(
+			_FILE_SYSTEM_STORE, null, invalidVersionFile, expectedMessage1,
+			expectedMessage2);
+	}
+
+	@Test
+	public void testFileSystemStoreWithValidStructure() throws Exception {
+		Path validDirectory = Paths.get(
+			_fileSystemStoreRootDir, String.valueOf(_companyId),
+			String.valueOf(_repositoryId), "100", "1.0");
+
+		_assertVerify(_FILE_SYSTEM_STORE, validDirectory, null);
+	}
+
+	@Override
+	protected VerifyProcess getVerifyProcess() {
+		return new PreupgradeVerifyFileSystemStoreStructure();
+	}
+
+	private void _assertVerify(
+			String storeImpl, Path directoryPath, Path filePath,
+			String... expectedMessages)
+		throws Exception {
+
+		String originalDLStoreImpl = ReflectionTestUtil.getAndSetFieldValue(
+			PropsValues.class, "DL_STORE_IMPL", storeImpl);
+
+		LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+			PreupgradeVerifyFileSystemStoreStructure.class.getName(),
+			LoggerTestUtil.ERROR);
+
+		try {
+			if (directoryPath != null) {
+				Files.createDirectories(directoryPath);
+			}
+
+			if (filePath != null) {
+				Files.createDirectories(filePath.getParent());
+				Files.createFile(filePath);
+			}
+
+			super.testVerify();
+
+			_validateLogEntries(logCapture, expectedMessages.length);
+
+			if (expectedMessages.length > 0) {
+				Assert.fail();
+			}
+		}
+		catch (Exception exception) {
+			String expectedType;
+			Path rootDirPath;
+
+			if (_ADVANCED_FILE_SYSTEM_STORE.equals(storeImpl)) {
+				expectedType = "AdvancedFileSystemStore";
+				rootDirPath = Paths.get(_advancedFileSystemStoreRootDir);
+			}
+			else {
+				expectedType = "FileSystemStore";
+				rootDirPath = Paths.get(_fileSystemStoreRootDir);
+			}
+
+			String expectedExceptionMessage = StringBundler.concat(
+				"File system store directory structure mismatch. Expected ",
+				expectedType, " structure but found invalid structure in: ",
+				rootDirPath);
+
+			Assert.assertEquals(
+				expectedExceptionMessage, exception.getMessage());
+
+			_validateLogEntries(
+				logCapture, expectedMessages.length, expectedMessages);
+		}
+		finally {
+			logCapture.close();
+
+			ReflectionTestUtil.setFieldValue(
+				PropsValues.class, "DL_STORE_IMPL", originalDLStoreImpl);
+		}
+	}
+
+	private void _validateLogEntries(
+		LogCapture logCapture, int expectedCount, String... expectedMessages) {
+
+		List<LogEntry> logEntries = logCapture.getLogEntries();
+
+		Assert.assertEquals(
+			logEntries.toString(), expectedCount, logEntries.size());
+
+		for (int i = 0; i < expectedMessages.length; i++) {
+			Assert.assertEquals(
+				expectedMessages[i],
+				logEntries.get(
+					i
+				).getMessage());
+		}
+	}
+
+	private static final String _ADVANCED_FILE_SYSTEM_STORE =
+		"com.liferay.portal.store.file.system.AdvancedFileSystemStore";
+
+	private static final String _FILE_SYSTEM_STORE =
+		"com.liferay.portal.store.file.system.FileSystemStore";
+
+	private static Configuration _advancedFileSystemStoreConfiguration;
+	private static String _advancedFileSystemStoreRootDir;
+	private static long _companyId;
+
+	@Inject
+	private static ConfigurationAdmin _configurationAdmin;
+
+	private static Configuration _fileSystemStoreConfiguration;
+	private static String _fileSystemStoreRootDir;
+
+	@Inject
+	private static Props _props;
+
+	private static long _repositoryId;
+	private static SafeCloseable
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable;
+
+}
---
+++ modules/apps/portal/portal-verify-test/src/testIntegration/java/com/liferay/portal/verify/test/PreupgradeVerifyFileSystemStoreWritePermissionTest.java
@@ -0,0 +1,127 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.verify.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.verify.PreupgradeVerifyFileSystemStoreWritePermission;
+import com.liferay.portal.verify.VerifyProcess;
+import com.liferay.portal.verify.test.util.BaseVerifyProcessTestCase;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author István András Dézsi
+ */
+@RunWith(Arquillian.class)
+public class PreupgradeVerifyFileSystemStoreWritePermissionTest
+	extends BaseVerifyProcessTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_rootDir =
+			_props.get(PropsKeys.LIFERAY_HOME) + "/test/store/file_system";
+
+		_configuration = _configurationAdmin.getConfiguration(
+			"com.liferay.portal.store.file.system.configuration." +
+				"FileSystemStoreConfiguration",
+			StringPool.QUESTION);
+
+		ConfigurationTestUtil.saveConfiguration(
+			_configuration,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"rootDir", _rootDir
+			).build());
+
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable =
+			PropsValuesTestUtil.swapWithSafeCloseable(
+				"UPGRADE_DATABASE_DL_STORAGE_CHECK_DISABLED", false);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		ConfigurationTestUtil.deleteConfiguration(_configuration);
+
+		FileUtil.deltree(_rootDir);
+
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable.close();
+	}
+
+	@Test
+	public void testVerifyNonexistentRootDirectory() throws Exception {
+		Path originalPath = Paths.get(_rootDir);
+
+		Path renamedPath = Paths.get(
+			StringUtil.replace(_rootDir, "file_system", "file_system2"));
+
+		if (Files.exists(originalPath)) {
+			Files.move(originalPath, renamedPath);
+		}
+
+		try {
+			super.testVerify();
+
+			Assert.fail();
+		}
+		catch (Exception exception) {
+			Assert.assertEquals(
+				"File system store directory does not exist: " + originalPath,
+				exception.getMessage());
+		}
+		finally {
+			if (Files.exists(renamedPath)) {
+				Files.move(renamedPath, originalPath);
+			}
+		}
+	}
+
+	@Override
+	protected VerifyProcess getVerifyProcess() {
+		return new PreupgradeVerifyFileSystemStoreWritePermission();
+	}
+
+	private static Configuration _configuration;
+
+	@Inject
+	private static ConfigurationAdmin _configurationAdmin;
+
+	@Inject
+	private static Props _props;
+
+	private static String _rootDir;
+	private static SafeCloseable
+		_upgradeDatabaseDLStorageCheckDisabledSafeCloseable;
+
+}
