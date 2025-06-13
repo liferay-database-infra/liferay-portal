e17857e80eec851eacf703d5ee2389dbd403e20d
IstvanD <istvan.dezsi@liferay.com>
2025-06-13 11:45:30 +0200

    LPD-53149 Verifying file system store structure and permissions

--- portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProcessSuite.java
+++ portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProcessSuite.java
@@ -24,6 +24,8 @@ public class PreupgradeVerifyProcessSuite extends PreupgradeVerifyProcess {
 		_verify(new PreupgradeVerifyCompanyUsers());
 		_verify(new PreupgradeVerifyDatabaseCharacterSet());
 		_verify(new PreupgradeVerifyDatabaseState());
+		_verify(new PreupgradeVerifyFileSystemStoreStructure());
+		_verify(new PreupgradeVerifyFileSystemStoreWritePermission());
 		_verify(new PreupgradeVerifyProperties());
 
 		if (ListUtil.isNotEmpty(_exceptionMessages)) {
