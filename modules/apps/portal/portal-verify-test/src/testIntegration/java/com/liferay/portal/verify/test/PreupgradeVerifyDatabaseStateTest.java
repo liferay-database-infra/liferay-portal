e1455df9018cb3da71830611a446df0158851ebc
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-28 15:35:36 +0200

    LPD-59133 Create test to validate missing views.

--- modules/apps/portal/portal-verify-test/src/testIntegration/java/com/liferay/portal/verify/test/PreupgradeVerifyDatabaseStateTest.java
+++ modules/apps/portal/portal-verify-test/src/testIntegration/java/com/liferay/portal/verify/test/PreupgradeVerifyDatabaseStateTest.java
@@ -9,7 +9,10 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.db.DBResourceUtil;
+import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.db.partition.DBPartition;
 import com.liferay.portal.kernel.instance.PortalInstancePool;
@@ -21,6 +24,7 @@ import com.liferay.portal.kernel.service.ServiceComponentLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.version.Version;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -37,6 +41,7 @@ import java.util.TreeSet;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -91,6 +96,39 @@ public class PreupgradeVerifyDatabaseStateTest
 	}
 
 	@Test
+	public void testVerifyPreupgradeMissing() throws SQLException {
+		ServiceComponent serviceComponent =
+			_serviceComponentLocalService.createServiceComponent(
+				RandomTestUtil.nextLong());
+
+		DBInspector dbInspector = new DBInspector(DataAccess.getConnection());
+
+		String tableName = dbInspector.normalizeName("TestTable");
+
+		serviceComponent.setMvccVersion(0);
+		serviceComponent.setBuildNamespace("com.liferay.test.service.impl");
+		serviceComponent.setData(
+			StringBundler.concat("<![CDATA[create table ", tableName, " ("));
+
+		_serviceComponentLocalService.addServiceComponent(serviceComponent);
+
+		try {
+			testVerify();
+
+			Assert.fail();
+		}
+		catch (Exception exception) {
+			Assert.assertEquals(
+				exception.getMessage(),
+				"Missing tables detected: [" + tableName + "]");
+		}
+		finally {
+			_serviceComponentLocalService.deleteServiceComponent(
+				serviceComponent);
+		}
+	}
+
+	@Test
 	public void testVerifyPreupgradeMissingTable() throws SQLException {
 		ServiceComponent serviceComponent =
 			_serviceComponentLocalService.createServiceComponent(
@@ -120,6 +158,34 @@ public class PreupgradeVerifyDatabaseStateTest
 		finally {
 			_serviceComponentLocalService.deleteServiceComponent(
 				serviceComponent);
+		}
+	}
+
+	@Test
+	public void testVerifyPreupgradeMissingView() throws Exception {
+		Assume.assumeTrue(DBPartition.isPartitionEnabled());
+
+		_renameView("Release_", "Release_backup");
+
+		try {
+			testVerify();
+
+			Assert.fail();
+		}
+		catch (Exception exception) {
+			DBInspector dbInspector = new DBInspector(
+				DataAccess.getConnection());
+
+			String viewName = dbInspector.normalizeName("Release_");
+
+			Assert.assertEquals(
+				StringBundler.concat(
+					"Missing views detected: [", viewName, "] in company ",
+					TestPropsValues.getCompanyId()),
+				exception.getMessage());
+		}
+		finally {
+			_renameView("Release_backup", "Release_");
 		}
 	}
 
@@ -179,6 +245,29 @@ public class PreupgradeVerifyDatabaseStateTest
 		}
 
 		return null;
+	}
+
+	private void _renameView(String fromViewName, String toViewName)
+		throws Exception {
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					TestPropsValues.getCompanyId())) {
+
+			DB db = DBManagerUtil.getDB();
+
+			if (db.getDBType() == DBType.MYSQL) {
+				db.runSQL(
+					StringBundler.concat(
+						"rename table ", fromViewName, " to ", toViewName));
+			}
+			else {
+				db.runSQL(
+					StringBundler.concat(
+						"alter view ", fromViewName, " rename to ",
+						toViewName));
+			}
+		}
 	}
 
 	private static final Version _TEST_SCHEMA_VERSION = new Version(0, 0, 0);
