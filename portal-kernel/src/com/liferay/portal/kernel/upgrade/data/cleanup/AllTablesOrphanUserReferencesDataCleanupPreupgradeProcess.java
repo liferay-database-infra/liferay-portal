a1167eaf29f8ea8dbc2cc0e5bf60c5af3cd8736d
Luis Ortiz <luis.ortiz@liferay.com>
2025-07-23 13:50:23 +0200

    LPD-53134 New data cleanup preupgrade process that browses all the tables to replace the inexistent userId values by an existing userId

---
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/data/cleanup/AllTablesOrphanUserReferencesDataCleanupPreupgradeProcess.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.upgrade.data.cleanup;
+
+import com.liferay.portal.kernel.upgrade.data.cleanup.util.OrphanReferencesDataCleanupUtil;
+
+/**
+ * @author Luis Ortiz
+ */
+public class AllTablesOrphanUserReferencesDataCleanupPreupgradeProcess
+	extends BaseAllTablesOrphanReferencesDataCleanupPreupgradeProcess {
+
+	public AllTablesOrphanUserReferencesDataCleanupPreupgradeProcess() {
+		super("userId", "User_");
+	}
+
+	@Override
+	protected void executeLogic(
+			String sourceColumnName, String sourceTableName,
+			String targetColumnName, String targetTableName)
+		throws Exception {
+
+		OrphanReferencesDataCleanupUtil.fixOrphanUsers(
+			connection, sourceColumnName, sourceTableName, targetColumnName,
+			targetTableName);
+	}
+
+}
