e6b35371235cfcc39593e51f989dec9aa2dfad28
Luis Ortiz <luis.ortiz@liferay.com>
2025-08-05 17:24:34 +0200

    LPD-53135 New data cleanup preupgrade process to delete the content of the AnalyticsMessage table

---
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/AnalyticsMessageDataCleanupPreupgradeProcess.java
@@ -0,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.data.cleanup;
+
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.data.cleanup.DataCleanupPreupgradeProcess;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Luis Ortiz
+ */
+public class AnalyticsMessageDataCleanupPreupgradeProcess
+	extends DataCleanupPreupgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		DBInspector dbInspector = new DBInspector(connection);
+
+		if (!dbInspector.hasTable("AnalyticsMessage")) {
+			return;
+		}
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"delete from AnalyticsMessage")) {
+
+			int rowCount = preparedStatement.executeUpdate();
+
+			if (rowCount > 0) {
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"Deleted content of table " +
+							dbInspector.normalizeName("AnalyticsMessage"));
+				}
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AnalyticsMessageDataCleanupPreupgradeProcess.class);
+
+}
