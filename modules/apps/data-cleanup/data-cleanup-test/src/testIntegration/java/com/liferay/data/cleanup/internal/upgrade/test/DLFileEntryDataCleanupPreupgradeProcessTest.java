977a037b5787249e5c0286ba6066954566a23b13
IstvanD <istvan.dezsi@liferay.com>
2025-08-25 11:40:23 +0200

    LPD-59797 Testing a DataCleanupPreupgradeProcesses triggered by DataRemovalConfiguration change

---
+++ modules/apps/data-cleanup/data-cleanup-test/src/testIntegration/java/com/liferay/data/cleanup/internal/upgrade/test/DLFileEntryDataCleanupPreupgradeProcessTest.java
@@ -0,0 +1,115 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.data.cleanup.internal.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.data.cleanup.DLFileEntryDataCleanupPreupgradeProcess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author István András Dézsi
+ */
+@RunWith(Arquillian.class)
+public class DLFileEntryDataCleanupPreupgradeProcessTest
+	extends DLFileEntryDataCleanupPreupgradeProcess {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testRemoveDLFileEntryOrphanData() throws Exception {
+		long fileEntryId1 = RandomTestUtil.nextLong();
+		long fileEntryId2 = RandomTestUtil.nextLong();
+
+		runSQL(
+			StringBundler.concat(
+				"insert into DLFileEntry (",
+				"mvccVersion, ctCollectionId, fileEntryId, groupId) values ",
+				"(0, 0, ", fileEntryId1, ", ", RandomTestUtil.nextLong(), ")"));
+		runSQL(
+			StringBundler.concat(
+				"insert into DLFileEntry (",
+				"mvccVersion, ctCollectionId, fileEntryId, groupId, name) ",
+				"values (0, 0, ", fileEntryId2, ", ", RandomTestUtil.nextLong(),
+				", '')"));
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				DLFileEntryDataCleanupPreupgradeProcess.class.getName(),
+				LoggerTestUtil.INFO);
+			ConfigurationTemporarySwapper configurationTemporarySwapper =
+				new ConfigurationTemporarySwapper(
+					"com.liferay.data.cleanup.internal.configuration." +
+						"DataRemovalConfiguration",
+					HashMapDictionaryBuilder.<String, Object>put(
+						"removeDLFileEntryOrphanData", true
+					).build())) {
+
+			_assertDLFileEntryNotExists(fileEntryId1);
+			_assertDLFileEntryNotExists(fileEntryId2);
+
+			List<String> messages = logCapture.getMessages();
+
+			Assert.assertTrue(
+				messages.contains(
+					"Deleted document library file entry " + fileEntryId1 +
+						" because its name was null"));
+			Assert.assertTrue(
+				messages.contains(
+					StringBundler.concat(
+						"Deleted document library file entry ", fileEntryId2,
+						" because its name was ",
+						(DBManagerUtil.getDBType() == DBType.ORACLE) ? "null" :
+							"empty")));
+		}
+		finally {
+			runSQL(
+				"delete from DLFileEntry where fileEntryId = " + fileEntryId1);
+			runSQL(
+				"delete from DLFileEntry where fileEntryId = " + fileEntryId2);
+		}
+	}
+
+	private void _assertDLFileEntryNotExists(long fileEntryId)
+		throws Exception {
+
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				"select count(*) from DLFileEntry where fileEntryId = ?")) {
+
+			preparedStatement.setLong(1, fileEntryId);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				Assert.assertTrue(resultSet.next());
+				Assert.assertEquals(0, resultSet.getInt(1));
+			}
+		}
+	}
+
+}
