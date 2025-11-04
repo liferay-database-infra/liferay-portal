586abc79daaea708ebe29187619075eaf93391fe
Jorge Avalos <jorge.avalos@liferay.com>
2025-11-04 15:41:38 -0800

    LPD-69531 test

---
+++ modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/dto/v1_0/DisplayPageFormSubmissionResult.java
@@ -0,0 +1,128 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.delivery.client.dto.v1_0;
+
+import com.liferay.headless.delivery.client.function.UnsafeSupplier;
+import com.liferay.headless.delivery.client.serdes.v1_0.DisplayPageFormSubmissionResultSerDes;
+
+import jakarta.annotation.Generated;
+
+import java.io.Serializable;
+
+import java.util.Objects;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+public class DisplayPageFormSubmissionResult
+	implements Cloneable, Serializable {
+
+	public static DisplayPageFormSubmissionResult toDTO(String json) {
+		return DisplayPageFormSubmissionResultSerDes.toDTO(json);
+	}
+
+	public Mapping getMapping() {
+		return mapping;
+	}
+
+	public void setMapping(Mapping mapping) {
+		this.mapping = mapping;
+	}
+
+	public void setMapping(
+		UnsafeSupplier<Mapping, Exception> mappingUnsafeSupplier) {
+
+		try {
+			mapping = mappingUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Mapping mapping;
+
+	public FragmentInlineValue getNotificationText() {
+		return notificationText;
+	}
+
+	public void setNotificationText(FragmentInlineValue notificationText) {
+		this.notificationText = notificationText;
+	}
+
+	public void setNotificationText(
+		UnsafeSupplier<FragmentInlineValue, Exception>
+			notificationTextUnsafeSupplier) {
+
+		try {
+			notificationText = notificationTextUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected FragmentInlineValue notificationText;
+
+	public Boolean getShowNotification() {
+		return showNotification;
+	}
+
+	public void setShowNotification(Boolean showNotification) {
+		this.showNotification = showNotification;
+	}
+
+	public void setShowNotification(
+		UnsafeSupplier<Boolean, Exception> showNotificationUnsafeSupplier) {
+
+		try {
+			showNotification = showNotificationUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Boolean showNotification;
+
+	@Override
+	public DisplayPageFormSubmissionResult clone()
+		throws CloneNotSupportedException {
+
+		return (DisplayPageFormSubmissionResult)super.clone();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof DisplayPageFormSubmissionResult)) {
+			return false;
+		}
+
+		DisplayPageFormSubmissionResult displayPageFormSubmissionResult =
+			(DisplayPageFormSubmissionResult)object;
+
+		return Objects.equals(
+			toString(), displayPageFormSubmissionResult.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		return DisplayPageFormSubmissionResultSerDes.toJSON(this);
+	}
+
+}
