2418d2061ff5a971b7031143eb92c5eaf975b6d4
IstvanD <istvan.dezsi@liferay.com>
2025-11-20 10:32:37 +0100

    LPD-71089 Dropping object tables that belong to a deleted company

--- portal-impl/src/com/liferay/portal/upgrade/data/cleanup/CompanyDataCleanupPreupgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/CompanyDataCleanupPreupgradeProcess.java
@@ -5,10 +5,22 @@
 
 package com.liferay.portal.upgrade.data.cleanup;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.data.cleanup.DataCleanupPreupgradeProcess;
 import com.liferay.portal.kernel.upgrade.data.cleanup.DefaultAllTablesOrphanReferencesDataCleanupPreupgradeProcess;
 import com.liferay.portal.kernel.upgrade.data.cleanup.TableOrphanReferencesDataCleanupPreupgradeProcess;
+import com.liferay.portal.kernel.upgrade.data.cleanup.util.DataCleanupLoggingUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Luis Ortiz
@@ -18,6 +30,8 @@ public class CompanyDataCleanupPreupgradeProcess
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		_dropOrphanObjectTables();
+
 		upgrade(
 			new DefaultAllTablesOrphanReferencesDataCleanupPreupgradeProcess(
 				"companyId", "Company"));
@@ -32,5 +46,54 @@ public class CompanyDataCleanupPreupgradeProcess
 					PortletKeys.PREFS_OWNER_TYPE_COMPANY,
 				"ownerId", "PortletPreferences", "companyId", "Company"));
 	}
+
+	private void _dropOrphanObjectTables() throws Exception {
+		Set<Long> companyIds = SetUtil.fromArray(
+			PortalInstancePool.getCompanyIds());
+
+		DBInspector dbInspector = new DBInspector(connection);
+
+		List<String> tableNames = dbInspector.getTableNames(null);
+
+		for (String tableName : tableNames) {
+			long tableNameCompanyId = _getCompanyIdFromTableName(tableName);
+
+			if ((tableNameCompanyId > 0) &&
+				!companyIds.contains(tableNameCompanyId)) {
+
+				dropTable(tableName);
+
+				DataCleanupLoggingUtil.logDrop(
+					_log, tableName,
+					"it belonged to a nonexistent company: " +
+						tableNameCompanyId);
+			}
+		}
+	}
+
+	private long _getCompanyIdFromTableName(String tableName) {
+		String[] tableNameParts = StringUtil.split(
+			tableName, StringPool.UNDERLINE);
+
+		if ((tableNameParts.length > 1) &&
+			(StringUtil.equalsIgnoreCase(tableNameParts[0], "l") ||
+			 StringUtil.equalsIgnoreCase(tableNameParts[0], "o"))) {
+
+			return GetterUtil.getLong(tableNameParts[1]);
+		}
+
+		if ((tableNameParts.length > 2) &&
+			StringUtil.equalsIgnoreCase(
+				tableNameParts[tableNameParts.length - 2], "x")) {
+
+			return GetterUtil.getLong(
+				tableNameParts[tableNameParts.length - 1]);
+		}
+
+		return -1;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CompanyDataCleanupPreupgradeProcess.class);
 
 }
