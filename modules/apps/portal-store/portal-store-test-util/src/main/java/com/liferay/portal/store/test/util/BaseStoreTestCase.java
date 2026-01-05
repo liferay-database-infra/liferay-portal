81fee87a155efaad7df8d7af5765b7c6ce3666be
Jorge Avalos <jorge.avalos@liferay.com>
2026-01-05 01:17:50 -0800

    LPD-75033 add integration test

--- modules/apps/portal-store/portal-store-test-util/src/main/java/com/liferay/portal/store/test/util/BaseStoreTestCase.java
+++ modules/apps/portal-store/portal-store-test-util/src/main/java/com/liferay/portal/store/test/util/BaseStoreTestCase.java
@@ -53,6 +53,23 @@ public abstract class BaseStoreTestCase {
 	}
 
 	@Test
+	public void testDeleteCompany() throws Exception {
+		String dirName = RandomTestUtil.randomString();
+
+		String fileName1 = dirName + "/" + RandomTestUtil.randomString();
+
+		_store.addFile(
+			_companyId, _repositoryId, fileName1, Store.VERSION_DEFAULT,
+			new UnsyncByteArrayInputStream(DATA_VERSION));
+
+		_store.deleteCompany(_companyId);
+
+		Assert.assertFalse(
+			_store.hasFile(
+				_companyId, _repositoryId, fileName1, Store.VERSION_DEFAULT));
+	}
+
+	@Test
 	public void testDeleteDirectory() throws Exception {
 		String dirName = RandomTestUtil.randomString();
 
