b96032c55b3ced5bfb3651f9e3a3b63b9dae5795
Jorge Avalos <jorge.avalos@liferay.com>
2026-01-06 11:40:46 -0800

    LPD-50167 add integration test

---
+++ modules/apps/portal-instances/portal-instances-test/src/testIntegration/java/com/liferay/portal/instances/internal/instance/lifecycle/test/StorePortalInstanceLifecycleListenerTest.java
@@ -0,0 +1,75 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.instances.internal.instance.lifecycle.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.store.Store;
+import com.liferay.petra.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jorge Avalos
+ */
+@RunWith(Arquillian.class)
+public class StorePortalInstanceLifecycleListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(new LiferayIntegrationTestRule());
+
+	@Test
+	public void testPortalInstanceUnregistered() throws Exception {
+		Company company = CompanyTestUtil.addCompany();
+
+		String fileName1 = "/" + RandomTestUtil.randomString();
+
+		long repositoryId = RandomTestUtil.nextLong();
+
+		_store.addFile(
+			company.getCompanyId(), repositoryId, fileName1,
+			Store.VERSION_DEFAULT, new UnsyncByteArrayInputStream(new byte[0]));
+
+		Assert.assertTrue(
+			_store.hasFile(
+				company.getCompanyId(), repositoryId, fileName1,
+				Store.VERSION_DEFAULT));
+
+		_portalInstanceLifecycleListener.portalInstanceUnregistered(company);
+
+		Assert.assertFalse(
+			_store.hasFile(
+				company.getCompanyId(), repositoryId, fileName1,
+				Store.VERSION_DEFAULT));
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject(
+		filter = "component.name=com.liferay.portal.instances.internal.instance.lifecycle.StorePortalInstanceLifecycleListener"
+	)
+	private PortalInstanceLifecycleListener _portalInstanceLifecycleListener;
+
+	@Inject(
+		filter = "(&(objectClass=com.liferay.document.library.kernel.store.Store)(default=true))"
+	)
+	private Store _store;
+
+}
