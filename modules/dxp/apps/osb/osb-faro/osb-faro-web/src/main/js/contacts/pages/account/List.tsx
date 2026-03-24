f51f963ab0df510b4a9be0461762197c35cb87d9
Tiago Campêlo <tiago.campelo@liferay.com>
2026-03-24 17:05:35 -0300

    LPD-83733 Use FDS into Account list component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/List.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/List.tsx
@@ -1,40 +1,11 @@
-import * as API from 'shared/api';
-import BaseListPage from 'contacts/components/BaseListPage';
-import ClayLink from '@clayui/link';
+import * as breadcrumbs from 'shared/util/breadcrumbs';
+import BasePage from 'shared/components/base-page';
+import Card from 'shared/components/Card';
 import React from 'react';
-import URLConstants from 'shared/util/url-constants';
-import {
-	ACCOUNT_TYPE,
-	ACTIVITIES_COUNT,
-	createOrderIOMap,
-	getDefaultSortOrder,
-	INDIVIDUAL_COUNT,
-	NAME
-} from 'shared/util/pagination';
-import {accountsListColumns} from 'shared/util/table-columns';
-import {FetchSegmentsParams} from 'segment/pages/List';
-import {Routes, toRoute} from 'shared/util/router';
-import {Sizes} from 'shared/util/constants';
+import {useChannelContext} from 'shared/context/channel';
 import {useCurrentUser} from 'shared/hooks/useCurrentUser';
-import {useQueryPagination} from 'shared/hooks/useQueryPagination';
+import {useFrontendDataSet} from 'shared/hooks/useFrontendDataSet';
 import {User} from 'shared/util/records';
-
-const getAccountsDataSource = ({
-	channelId,
-	delta,
-	groupId,
-	orderIOMap,
-	page,
-	query
-}: FetchSegmentsParams) =>
-	API.accounts.search({
-		channelId,
-		delta,
-		groupId,
-		orderIOMap,
-		page,
-		query
-	});
 
 interface IListProps {
 	channelId: string;
@@ -44,97 +15,158 @@ interface IListProps {
 
 const List: React.FC<IListProps> = ({channelId, groupId, ...otherProps}) => {
 	const currentUser = useCurrentUser();
+	const {selectedChannel} = useChannelContext();
+
 	const authorized = currentUser.isAdmin();
 
-	const columns = [
-		accountsListColumns.getName({channelId, groupId}),
-		accountsListColumns.type,
-		accountsListColumns.individualCount,
-		accountsListColumns.activitiesCount
-	];
-
-	const {delta, orderIOMap, page, query} = useQueryPagination({
-		initialOrderIOMap: createOrderIOMap(NAME, getDefaultSortOrder(NAME))
-	});
+	const FrontendDataSet = useFrontendDataSet();
 
 	return (
-		<BaseListPage
-			{...otherProps}
-			columns={columns}
-			currentUser={currentUser}
-			dataSourceFn={getAccountsDataSource}
-			delta={delta}
-			emptyStateTitle={Liferay.Language.get(
-				'no-accounts-synced-from-data-sources'
-			)}
-			entityLabel={Liferay.Language.get('accounts')}
-			noResultsConfig={{
-				description: (
-					<>
-						{Liferay.Language.get(
-							'connect-a-data-source-to-get-started'
-						)}
-
-						<ClayLink
-							className='d-block mb-3'
-							href={URLConstants.DataSourceConnection}
-							key='DOCUMENTATION'
-							target='_blank'
-						>
-							{Liferay.Language.get(
-								'access-our-documentation-to-learn-more'
-							)}
-						</ClayLink>
-
-						{authorized && (
-							<ClayLink
-								button
-								className='button-root'
-								displayType='primary'
-								href={toRoute(
-									Routes.SETTINGS_DATA_SOURCE_LIST,
-									{
-										groupId
-									}
-								)}
-							>
-								{Liferay.Language.get('connect-data-source')}
-							</ClayLink>
-						)}
-					</>
-				),
-				icon: {
-					border: false,
-					size: Sizes.XXXLarge,
-					symbol: 'ac_satellite'
-				},
-				title: Liferay.Language.get(
-					'no-accounts-synced-from-data-sources'
-				)
-			}}
-			orderByOptions={[
-				{
-					label: Liferay.Language.get('account-name'),
-					value: NAME
-				},
-				{
-					label: Liferay.Language.get('account-type'),
-					value: ACCOUNT_TYPE
-				},
-				{
-					label: Liferay.Language.get('individuals'),
-					value: INDIVIDUAL_COUNT
-				},
-				{
-					label: Liferay.Language.get('total-activities'),
-					value: ACTIVITIES_COUNT
-				}
-			]}
-			orderIOMap={orderIOMap}
-			page={page}
-			query={query}
-			rowIdentifier='id'
-		/>
+		<BasePage documentTitle={Liferay.Language.get('accounts')}>
+			<BasePage.Header
+				breadcrumbs={[
+					breadcrumbs.getHome({
+						channelId,
+						groupId,
+						label: selectedChannel && selectedChannel.name
+					})
+				]}
+				groupId={groupId}
+			>
+				<BasePage.Row>
+					<BasePage.Header.TitleSection
+						title={Liferay.Language.get('accounts')}
+					/>
+				</BasePage.Row>
+			</BasePage.Header>
+			<BasePage.Body>
+				<Card>
+					{FrontendDataSet && (
+						<FrontendDataSet
+							// TODO => Use the correct endpoint
+							// apiURL={`o/contacts/${groupId}/account/search`}
+							apiURL='/o/headless-admin-user/v1.0/user-accounts'
+							customDataRenderers={{
+								testRenderer: ({value}) => (
+									<span>
+										<b>{value}</b>
+									</span>
+								)
+							}}
+							filters={[
+								{
+									id: 'lifecycleStatus',
+									items: [
+										{
+											label: Liferay.Language.get(
+												'established'
+											),
+											value: 'established'
+										},
+										{
+											label: Liferay.Language.get(
+												'pipeline'
+											),
+											value: 'pipeline'
+										},
+										{
+											label: Liferay.Language.get(
+												'at-risk'
+											),
+											value: 'at-risk'
+										}
+									],
+									label: Liferay.Language.get('status'),
+									name: 'status',
+									type: 'selection'
+								},
+								{
+									id: 'company',
+									items: [
+										{
+											label: Liferay.Language.get(
+												'liferay'
+											),
+											value: 'liferay'
+										}
+									],
+									label: Liferay.Language.get('industry'),
+									name: 'companyId',
+									type: 'selection'
+								}
+							]}
+							pagination={{
+								deltas: [{label: 10}, {label: 20}, {label: 50}],
+								initialDelta: 20,
+								initialPageNumber: 1
+							}}
+							showPagination
+							snapshotsEnabled
+							sort={[
+								{
+									active: true,
+									direction: 'asc',
+									key: 'alternateName',
+									label: Liferay.Language.get(
+										'alternate-name'
+									)
+								},
+								{
+									active: false,
+									direction: 'asc',
+									key: 'emailAddress',
+									label: Liferay.Language.get('email')
+								}
+							]}
+							views={[
+								{
+									contentRenderer: 'table',
+									default: false,
+									label: 'table',
+									name: 'table',
+									schema: {
+										fields: [
+											{
+												fieldName: 'alternateName',
+												label: Liferay.Language.get(
+													'alternate-name'
+												),
+												sortable: true,
+												truncate: true
+											},
+											{
+												fieldName: 'familyName',
+												label: Liferay.Language.get(
+													'family-name'
+												),
+												sortable: false
+											},
+											{
+												contentRenderer: 'testRenderer',
+												fieldName: 'id',
+												label: Liferay.Language.get(
+													'id'
+												),
+												sortable: false
+											},
+											{
+												contentRenderer: 'testRenderer',
+												fieldName: 'emailAddress',
+												label: Liferay.Language.get(
+													'email'
+												),
+												sortable: true
+											}
+										]
+									},
+									thumbnail: 'table'
+								}
+							]}
+						/>
+					)}
+				</Card>
+			</BasePage.Body>
+		</BasePage>
 	);
 };
 
