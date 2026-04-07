05df59f530abe42b75226e97a843aa6594b94ac0
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2026-04-07 16:12:26 +0200

    LPD-85346   Adapting classes to backport upgradeProcess

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v5_27_3/CPDefinitionSpecificationOptionValueUpgradeProcess.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v5_27_3/CPDefinitionSpecificationOptionValueUpgradeProcess.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.product.internal.upgrade.v5_27_2;
+package com.liferay.commerce.product.internal.upgrade.v5_27_3;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
