da65fd782b18a07ffec3870d2baf204af2af7610
IstvanD <istvan.dezsi@liferay.com>
2026-07-27 09:20:06 +0200

    LPD-96720 Test the Company partition migration

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/UpgradeCompanyDBPartitionTest.java
@@ -0,0 +1,162 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.v7_4_x.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.db.partition.util.DBPartitionUtil;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.v7_4_x.UpgradeCompanyDBPartition;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author István András Dézsi
+ */
+@RunWith(Arquillian.class)
+public class UpgradeCompanyDBPartitionTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpgradeReplacesCompanyView() throws Exception {
+		Assume.assumeTrue(PropsValues.DATABASE_PARTITION_ENABLED);
+
+		long companyId = TestPropsValues.getCompanyId();
+
+		long defaultCompanyId = PortalInstancePool.getDefaultCompanyId();
+
+		Assume.assumeTrue(companyId != defaultCompanyId);
+
+		String partitionName = DBPartitionUtil.getPartitionName(companyId);
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					defaultCompanyId);
+
+			Connection connection = DataAccess.getConnection();
+
+			Statement statement = connection.createStatement()) {
+
+			statement.executeUpdate("drop table " + partitionName + ".Company");
+			statement.executeUpdate(
+				StringBundler.concat(
+					"create or replace view ", partitionName,
+					".Company as select * from ",
+					DBPartitionUtil.getPartitionName(defaultCompanyId),
+					".Company"));
+		}
+
+		try {
+			_runUpgrade();
+
+			try (SafeCloseable safeCloseable =
+					CompanyThreadLocal.setCompanyIdWithSafeCloseable(companyId);
+
+				Connection connection = DataAccess.getConnection();
+
+				PreparedStatement preparedStatement =
+					connection.prepareStatement(
+						"select companyId from Company");
+
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				List<Long> companyIds = new ArrayList<>();
+
+				while (resultSet.next()) {
+					companyIds.add(resultSet.getLong("companyId"));
+				}
+
+				Assert.assertEquals(
+					Collections.singletonList(companyId), companyIds);
+			}
+		}
+		finally {
+			try (SafeCloseable safeCloseable =
+					CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+						defaultCompanyId);
+				Connection connection = DataAccess.getConnection()) {
+
+				DBPartitionUtil.replaceByTable(
+					connection, companyId, "Company", true,
+					" where companyId = " + companyId);
+			}
+
+			EntityCacheUtil.clearCache();
+			FinderCacheUtil.clearCache();
+		}
+	}
+
+	@Test
+	public void testUpgradeRetainsCompany() throws Exception {
+		Company company = _companyLocalService.getCompany(
+			TestPropsValues.getCompanyId());
+
+		Map<String, Object> modelAttributes = company.getModelAttributes();
+
+		int companiesCount = _companyLocalService.getCompaniesCount();
+
+		_runUpgrade();
+
+		_multiVMPool.clear();
+
+		company = _companyLocalService.getCompany(company.getCompanyId());
+
+		Assert.assertEquals(modelAttributes, company.getModelAttributes());
+
+		Assert.assertEquals(
+			companiesCount, _companyLocalService.getCompaniesCount());
+	}
+
+	private void _runUpgrade() throws Exception {
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					PortalInstancePool.getDefaultCompanyId())) {
+
+			UpgradeProcess upgradeProcess = new UpgradeCompanyDBPartition();
+
+			upgradeProcess.upgrade();
+		}
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+}
